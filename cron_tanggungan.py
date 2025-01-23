c6d580e3e3789c87b736c37867ea2330c8d68279
kentoespdam <kentoes.pdam@gmail.com>
2025-01-23 11:25:09 +0700

    update tanggungan pegawai done

--- cron_tanggungan.py
+++ cron_tanggungan.py
@@ -1,102 +1,44 @@
+from core.enums import STATUS_KAWIN, STATUS_PENDIDIKAN
+from core.pegawai import update_pegawai_tanggungan
+from core.profil_keluarga import fetch_tanggungan_list, update_tanggungan_status
 import pandas as pd
-from sqlalchemy import text
-
-from config import ENGINE
-from enums import HUBUNGAN_KELUARGA, STATUS_KAWIN, STATUS_KERJA, STATUS_PENDIDIKAN
+from icecream import ic
+import dask.dataframe as dd
 
 
-def update_tanggungan(pegawai_id: int = None) -> None:
-    """Update tanggungan status for each pegawai based on their anak's status."""
-    sql = """
-        SELECT
-            pk.id,
-            pk.biodata_id,
-            pk.tanggal_lahir,
-            pk.status_kawin,
-            pk.status_pendidikan,
-            TIMESTAMPDIFF(
-                YEAR,
-                pk.tanggal_lahir,
-                CURRENT_DATE
-            ) AS umur,
-            IF(
-                CURRENT_DATE > DATE_ADD(pk.tanggal_lahir, INTERVAL 21 YEAR),
-                TRUE,
-                FALSE
-            ) AS gt21,
-            IF(
-                CURRENT_DATE > DATE_ADD(pk.tanggal_lahir, INTERVAL 26 YEAR),
-                TRUE,
-                FALSE
-            ) AS gt26
-        FROM
-            profil_keluarga pk
-            INNER JOIN pegawai p
-                ON pk.biodata_id = p.biodata_id
-                AND p.is_deleted = FALSE
-                AND p.status_kerja IN (%s, %s)
-        WHERE
-            pk.is_deleted = FALSE
-            AND pk.hubungan_keluarga = %s
-            AND tanggungan = TRUE
-        """
-    params = [
-        STATUS_KERJA.KARYAWAN_AKTIF.value,
-        STATUS_KERJA.DIRUMAHKAN.value,
-        HUBUNGAN_KELUARGA.ANAK.value,
-    ]
-    if pegawai_id:
-        sql += " AND p.id = %s"
-        params.append(pegawai_id)
+def calculate_tanggungan(tanggungan_list: pd.DataFrame):
+    tanggungan_list.loc[:, "tanggungan"] = ~(
+        (tanggungan_list.loc[:, "status_kawin"] == STATUS_KAWIN.KAWIN.value) |
+        (tanggungan_list.loc[:, "umur"] > 26) |
+        (
+            (tanggungan_list.loc[:, "umur"] > 21) &
+            (tanggungan_list.loc[:, "status_pendidikan"] ==
+             STATUS_PENDIDIKAN.SELESAI_SEKOLAH.value)
+        )
+    )
 
-    df = pd.read_sql(sql, con=ENGINE, params=tuple(params))
+    return tanggungan_list
 
-    for index, row in df.iterrows():
-        status_kawin = row["status_kawin"]
-        gt21 = row["gt21"]
-        gt26 = row["gt26"]
-        status_pendidikan = row["status_pendidikan"]
 
-        if (
-            status_kawin == STATUS_KAWIN.KAWIN.value
-            or gt26 == 1
-            or (
-                gt21 == 1
-                and status_pendidikan == STATUS_PENDIDIKAN.SELESAI_SEKOLAH.value
-            )
-        ):
-            # Release Tanggungan
-            with ENGINE.begin() as conn:
-                sql_update = text(
-                    """
-                    UPDATE profil_keluarga
-                    SET tanggungan = FALSE
-                    WHERE id = :id
-                    """
-                )
-                conn.execute(sql_update, parameters={"id": row["id"]})
-            df.drop(index, inplace=True)
+def main():
+    pd.options.mode.copy_on_write = True
+    tanggungan_df = pd.DataFrame(fetch_tanggungan_list())
+    tanggungan_df = dd.from_pandas(
+        tanggungan_df, npartitions=2
+    ).map_partitions(
+        calculate_tanggungan
+    ).compute()
 
-    df_grouped = df.groupby("biodata_id").agg({"id": "count"}).reset_index()
-    df_grouped = df_grouped.rename(columns={"id": "jml_tanggunan"})
+    update_tanggungan_status(tanggungan_df)
+    tanggungan_count_df = (
+        tanggungan_df[tanggungan_df["tanggungan"]]
+        .groupby("pegawai_id")
+        .size()
+        .reset_index(name="jumlah_tanggungan")
+    )
 
-    for index, row in df_grouped.iterrows():
-        with ENGINE.begin() as conn:
-            sql_update = text(
-                """
-                UPDATE pegawai
-                SET jml_tanggungan = :jml_tanggunan
-                WHERE biodata_id = :biodata_id
-                """
-            )
-            conn.execute(
-                sql_update,
-                parameters={
-                    "jml_tanggunan": row["jml_tanggunan"],
-                    "biodata_id": row["biodata_id"],
-                },
-            )
+    update_pegawai_tanggungan(tanggungan_count_df)
 
 
 if __name__ == "__main__":
-    update_tanggungan()
+    main()
