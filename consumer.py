dcac273a205f29406bf51ea72100fa5975be7b4d
kentoespdam <kentoes.pdam@gmail.com>
2025-01-23 11:37:05 +0700

    basic consumer

---
+++ consumer.py
@@ -0,0 +1,18 @@
+from kafka import KafkaConsumer
+from core.config import KAFKA_SERVER, PENGGAJIAN_TOPIC
+
+consumer = KafkaConsumer(
+    PENGGAJIAN_TOPIC, group_id=PENGGAJIAN_TOPIC, bootstrap_servers=KAFKA_SERVER
+)
+
+for message in consumer:
+    print(
+        "%s:%d:%d: key=%s value=%s"
+        % (
+            message.topic,
+            message.partition,
+            message.offset,
+            message.key,
+            message.value.decode("utf-8"),
+        )
+    )
